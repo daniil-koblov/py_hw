5bd4ca23d16a3910dfb76485e3dc4b21ab957c86
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-16 18:48:24 +0300

    third add py_hw_6_2.py

--- py_hw_6/py_hw_6_2.py
+++ py_hw_6/py_hw_6_2.py
@@ -12,3 +12,11 @@ for i in range(len(strn)):
     if strn[i] >= min and strn[i] <= max: stri.append(i)
 
 print(*stri)
+
+# ЭТАЛОННОЕ РЕШЕНИЕ:
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# min_number = int(input())
+# max_number = int(input())
+# for i in range(len(list_1)):
+#     if min_number <= list_1[i] <= max_number:
+#         print(i)
