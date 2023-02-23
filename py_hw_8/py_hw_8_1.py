b64f93fdd1b87e3d39aa861ea847c0961020d055
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-23 12:24:16 +0300

    first add file.txt

--- py_hw_8/py_hw_8_1.py
+++ py_hw_8/py_hw_8_1.py
@@ -4,14 +4,8 @@
 
 def add():
     with open('file.txt', 'a') as data:
-        surname = data.write(input('Введите фамилия: '))
-        data.write(' ')
-        name = data.write(input('Введите имя: '))
-        data.write(' ')
-        secondname = data.write(input('Введите отчество: '))
-        data.write(' ')
-        phonenumber = data.write(input('Введите номер телефона: '))
-        data.write('\n')
+        for i in range(int(input('Введите кол-во строк: '))):
+            data.write(input('Введите фамилия: ') + '\n'.upper())
     
 def output():
     path = 'file.txt'
@@ -28,15 +22,39 @@ def search(info):
         if info in line: print(line)
     data.close()
 
+def mod_del(info):
+    path = 'file.txt'
+    data = open('file.txt', 'r')
+    for line in data.readlines():
+        if info in line:
+            print(f'Вы хотите изменить\n'
+                    f'{line}\n'
+                    'в справочнике?')
+            if input().lower() == 'да':
+                y = input('Введите ФИО полностью и номер телефона: ')
+                data.read().replace(line, y)
+                data.close()
+                break
+            print(f'Вы хотите удалить\n'
+                    f'{line}\n'
+                    'из справочника?')
+            if input().lower() == 'да':
+                data.read().replace(line, '')
+                data.close()
+                break
+    data.close()
+
 print('Это программа для работы со справочником.\n'
         'Введите 1 для добавления нового абонента.\n'
         'Введите 2 для выведения всех абонентов.\n'
         'Введите 3 для поиска абонента.\n'
-        'Введите 4 для завершения работы программы.')
+        'Введите 4 для изменения или удаления искомых данных из справочника.\n'
+        'Введите 5 для завершения работы программы.')
 x = 0
-while x != 4:
+while x != 5:
     x = int(input('Введите выбранный вариант: '))
     if x == 1: add()
     elif x == 2: output()
     elif x == 3: search(input('Введите искомую информацию: '))  
-    elif x == 4: break
+    elif x == 4: mod_del(input('Введите искомую информацию: '))
+    elif x == 5: break
