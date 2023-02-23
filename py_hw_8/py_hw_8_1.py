3ca4d81ac31db5151d0856e8198d2ec5226cbc57
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-23 14:28:03 +0300

    second add py_hw_8_1.py

--- py_hw_8/py_hw_8_1.py
+++ py_hw_8/py_hw_8_1.py
@@ -3,58 +3,58 @@
 # для изменения и удаления данных.
 
 def add():
-    with open('file.txt', 'a') as data:
+    with open('file.txt', 'a', encoding = 'utf-8') as data:
         for i in range(int(input('Введите кол-во строк: '))):
-            data.write(input('Введите фамилия: ') + '\n'.upper())
+            data.write(input('Введите ФИО полностью и номер телефона: ').lower() + '\n')
     
 def output():
     path = 'file.txt'
-    data = open('file.txt', 'r')
+    data = open('file.txt', 'r', encoding = 'utf-8')
     for line in data:
         print(line)
     data.close()
 
 def search(info):
     path = 'file.txt'
-    data = open('file.txt', 'r')
+    data = open('file.txt', 'r', encoding = 'utf-8')
 
     for line in data.readlines():
         if info in line: print(line)
     data.close()
 
-def mod_del(info):
-    path = 'file.txt'
-    data = open('file.txt', 'r')
-    for line in data.readlines():
-        if info in line:
-            print(f'Вы хотите изменить\n'
-                    f'{line}\n'
-                    'в справочнике?')
-            if input().lower() == 'да':
-                y = input('Введите ФИО полностью и номер телефона: ')
-                data.read().replace(line, y)
-                data.close()
-                break
-            print(f'Вы хотите удалить\n'
-                    f'{line}\n'
-                    'из справочника?')
-            if input().lower() == 'да':
-                data.read().replace(line, '')
-                data.close()
-                break
-    data.close()
+def mod(info):
+    with open('file.txt') as data:
+        list_mod = []
+        for line in data:
+            if info in line.split(): list_mod.append(input(f'{line} --> ').lower() + '\n')
+            else: list_mod.append(line)
+    with open('file.txt', 'w') as data:
+        data.writelines(list_mod)
+        data.close()
+
+def delete(info):
+    with open('file.txt') as data:
+        list_del = []
+        for line in data:
+            if info in line: print(f'Данные по {line} удалены.')
+            else: list_del.append(line)
+    with open('file.txt', 'w') as data:
+        data.writelines(list_del)
+        data.close()
 
 print('Это программа для работы со справочником.\n'
         'Введите 1 для добавления нового абонента.\n'
         'Введите 2 для выведения всех абонентов.\n'
         'Введите 3 для поиска абонента.\n'
-        'Введите 4 для изменения или удаления искомых данных из справочника.\n'
-        'Введите 5 для завершения работы программы.')
+        'Введите 4 для изменения данных в справочнике.\n'
+        'Введите 5 для удаления данных в справочнике.\n'
+        'Введите 6 для завершения работы программы.')
 x = 0
-while x != 5:
+while x != 6:
     x = int(input('Введите выбранный вариант: '))
     if x == 1: add()
     elif x == 2: output()
-    elif x == 3: search(input('Введите искомую информацию: '))  
-    elif x == 4: mod_del(input('Введите искомую информацию: '))
-    elif x == 5: break
+    elif x == 3: search(input('Введите искомые данные: '))  
+    elif x == 4: mod(input('Введите данные для изменения: ').lower())
+    elif x == 5: delete(input('Введите данные для удаления: ').lower())
+    elif x == 6: break
